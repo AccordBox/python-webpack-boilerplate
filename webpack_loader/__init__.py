322210e308df7ea3a9d9cef1fe2809916dc28ff8
Michaelyin <hgyinjian@gmail.com>
2021-06-19 11:56:33 +0800

    release 0.0.3

--- webpack_loader/__init__.py
+++ webpack_loader/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Michael Yin"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 default_app_config = "webpack_loader.apps.WebpackLoaderConfig"
 GIT_URL = "https://github.com/AccordBox/python-webpack-boilerplate"
