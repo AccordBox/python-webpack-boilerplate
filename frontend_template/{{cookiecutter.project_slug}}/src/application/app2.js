20a8f332c5c89fc067453c8b35b6eaf02350782a
Michaelyin <hgyinjian@gmail.com>
2021-10-03 10:27:47 +0800

    update app.2

--- frontend_template/{{cookiecutter.project_slug}}/src/application/app2.js
+++ frontend_template/{{cookiecutter.project_slug}}/src/application/app2.js
@@ -1,11 +1,6 @@
-// This is the scss entry file
-import "../styles/index.scss";
+// import scss entry file here for app2.js
 
-// We can import Bootstrap JS instead of the CDN link, if you do not use
-// Bootstrap, please feel free to remove it.
-import "bootstrap/dist/js/bootstrap.bundle";
-
-// We can import other JS file as we like
+// import other JS components as we like
 import "../components/sidebar";
 
 window.document.addEventListener("DOMContentLoaded", function () {
