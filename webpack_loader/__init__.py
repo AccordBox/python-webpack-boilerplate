c15598f060678bf313e96f1a86bf54f5ba82a491
Michaelyin <hgyinjian@gmail.com>
2021-10-10 21:26:01 +0800

    use-local-frontend-template

--- webpack_loader/__init__.py
+++ webpack_loader/__init__.py
@@ -1,5 +1,1 @@
-__author__ = "Michael Yin"
-__version__ = "0.0.4"
-
 default_app_config = "webpack_loader.apps.WebpackLoaderConfig"
-GIT_URL = "https://github.com/AccordBox/python-webpack-boilerplate"
--- webpack_loader/management/commands/webpack_init.py
+++ webpack_loader/management/commands/webpack_init.py
@@ -1,9 +1,23 @@
+import os
+
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         from cookiecutter.main import cookiecutter
-        from webpack_loader import GIT_URL
 
-        cookiecutter(GIT_URL, directory="frontend_template")
+        root_pkg_path = os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        )
+        app_path = cookiecutter(root_pkg_path, directory="frontend_template")
+
+        app_name = os.path.basename(app_path)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Frontend app '{app_name}' "
+                f"has been successfully created. "
+                f"To know more, check https://python-webpack-boilerplate.rtfd.io/en/latest/frontend/"
+            )
+        )
