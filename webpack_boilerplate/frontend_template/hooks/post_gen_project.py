300929f1adb7ca15fb68f23b3bf47f116603ef4d
Michaelyin <michaelyin@accordbox.com>
2022-01-04 11:22:36 +0800

    run npm command at root

---
+++ webpack_boilerplate/frontend_template/hooks/post_gen_project.py
@@ -0,0 +1,54 @@
+import os
+import os.path
+import shutil
+
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m [WARNING]: "
+INFO = "\x1b[1;33m [INFO]: "
+HINT = "\x1b[3;33m"
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
+DENY_LIST = [".gitignore"]
+ALLOW_LIST = ["package.json", "package-lock.json"]
+
+
+def print_success_msg(msg):
+    print(SUCCESS + msg + TERMINATOR)
+
+
+def get_frontend_config_files():
+    frontend_path = os.getcwd()
+
+    for f in os.listdir(frontend_path):
+        if f.startswith(".") and f not in DENY_LIST:
+            full_path = os.path.join(frontend_path, f)
+            yield os.path.dirname(full_path), os.path.basename(full_path)
+
+    for f in ALLOW_LIST:
+        full_path = os.path.join(frontend_path, f)
+        yield os.path.dirname(full_path), os.path.basename(full_path)
+
+
+def copy_frontend_config_files():
+    for dirname, filename in get_frontend_config_files():
+        old_full_path = os.path.join(dirname, filename)
+        root_dir = os.path.dirname(dirname)
+        new_full_path = os.path.join(root_dir, filename)
+        shutil.copyfile(old_full_path, new_full_path)
+
+        os.remove(old_full_path)
+
+
+def main():
+    if "{{ cookiecutter.run_npm_command_at_root }}".lower() == "y":
+        copy_frontend_config_files()
+
+    print_success_msg(
+        f"Frontend app '{{ cookiecutter.project_slug }}' "
+        f"has been created. "
+        f"To know more, check https://python-webpack-boilerplate.rtfd.io/en/latest/frontend/"
+    )
+
+
+if __name__ == "__main__":
+    main()
--- webpack_boilerplate/management/commands/webpack_init.py
+++ webpack_boilerplate/management/commands/webpack_init.py
@@ -8,14 +8,4 @@ class Command(BaseCommand):
         from cookiecutter.main import cookiecutter
 
         pkg_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        app_path = cookiecutter(pkg_path, directory="frontend_template")
-
-        app_name = os.path.basename(app_path)
-
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"Frontend app '{app_name}' "
-                f"has been successfully created. "
-                f"To know more, check https://python-webpack-boilerplate.rtfd.io/en/latest/frontend/"
-            )
-        )
+        cookiecutter(pkg_path, directory="frontend_template")
