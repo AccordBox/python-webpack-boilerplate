1ccd68efda7dcd5e1a08ead98abbc9954fc7f903
Alex Clark <aclark@aclark.net>
2022-03-06 13:00:47 -0500

    Update webpack_init.py
    Add ``--no-input`` command line argument to suppress command line prompt. https://cookiecutter.readthedocs.io/en/2.0.2/advanced/suppressing_prompts.html#suppressing-command-line-prompts

--- webpack_boilerplate/management/commands/webpack_init.py
+++ webpack_boilerplate/management/commands/webpack_init.py
@@ -4,8 +4,18 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--no-input',
+            action='store_true',
+            help='Do not ask for input.',
+        )
+
     def handle(self, *args, **options):
         from cookiecutter.main import cookiecutter
-
         pkg_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        cookiecutter(pkg_path, directory="frontend_template")
+        if options['no_input']:
+            cookiecutter(pkg_path, directory="frontend_template", no_input=True)
+        else:
+        	cookiecutter(pkg_path, directory="frontend_template")
