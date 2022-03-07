616b20fa90cd3b8b260756cf3cc155a6af86f33a
Michaelyin <michaelyin@accordbox.com>
2022-03-07 10:07:49 +0800

    fix code style

--- webpack_boilerplate/management/commands/webpack_init.py
+++ webpack_boilerplate/management/commands/webpack_init.py
@@ -4,18 +4,16 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-
     def add_arguments(self, parser):
         parser.add_argument(
-            '--no-input',
-            action='store_true',
-            help='Do not ask for input.',
+            "--no-input", action="store_true", help="Do not ask for input.",
         )
 
     def handle(self, *args, **options):
         from cookiecutter.main import cookiecutter
+
         pkg_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        if options['no_input']:
+        if options["no_input"]:
             cookiecutter(pkg_path, directory="frontend_template", no_input=True)
         else:
             cookiecutter(pkg_path, directory="frontend_template")
